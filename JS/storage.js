6343c904a0807648a099d01da34a9f40f6d6b146
michaelarteaga <michael.arteaga.roman@gmail.com>
2026-01-05 15:13:29 -0500

    revision y cambios generales

--- JS/storage.js
+++ JS/storage.js
@@ -1,15 +1,17 @@
-// =======================
-// CARRITO - STORAGE
-// =======================
+// PRODUCTOS
+export function obtenerProductos() {
+  return JSON.parse(localStorage.getItem("productos")) || [];
+}
 
+export function guardarProductos(productos) {
+  localStorage.setItem("productos", JSON.stringify(productos));
+}
+
+// CARRITO
 export function obtenerCarrito() {
   return JSON.parse(localStorage.getItem("carrito")) || [];
 }
 
 export function guardarCarrito(carrito) {
   localStorage.setItem("carrito", JSON.stringify(carrito));
-}
-
-export function vaciarCarrito() {
-  localStorage.removeItem("carrito");
 }
